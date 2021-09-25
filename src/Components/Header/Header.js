6cd5e45d920dfaa2030adbc5cf931c25a1609d17
YHMunna <yeakubhossainmunna2017@gmail.com>
2021-09-26 02:00:59 +0600

    try

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <img
+          className="header-img"
+          src="https://www.saarc-sec.org/images/logot.svg"
+          alt=""
+        />
+      </div>
+      <div>
+        <h2>hey!</h2>
+        <h4>try</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
